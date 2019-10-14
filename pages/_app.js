1c7e86875ba6a28585444a8560c3c885b65f297f
Younes El Alami <shadoow.ma@gmail.com>
2019-10-14 12:16:37 +0800

    update dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import App, { Container } from 'next/app';
-import { ThemeProvider } from 'styled-components';
-import { ClientContext } from 'graphql-hooks';
+import React from "react";
+import App, { Container } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { ClientContext } from "graphql-hooks";
 
-import withGraphQLClient from 'lib/with-graphql-client';
-import { appWithTranslation } from 'lib/i18n';
+import withGraphQLClient from "lib/with-graphql-client";
+import { appWithTranslation } from "lib/i18n";
 
-import GlobalStyle from 'styles/GlobalStyle';
-import theme from 'styles/theme';
+import GlobalStyle from "styles/GlobalStyle";
+import theme from "styles/theme";
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -24,16 +24,14 @@ class MyApp extends App {
     const { Component, pageProps, graphQLClient } = this.props;
 
     return (
-      <Container>
-        <ThemeProvider theme={theme}>
-          <>
-            <GlobalStyle />
-            <ClientContext.Provider value={graphQLClient}>
-              <Component className={pageProps.theme} {...pageProps} />
-            </ClientContext.Provider>
-          </>
-        </ThemeProvider>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <ClientContext.Provider value={graphQLClient}>
+            <Component className={pageProps.theme} {...pageProps} />
+          </ClientContext.Provider>
+        </>
+      </ThemeProvider>
     );
   }
 }
